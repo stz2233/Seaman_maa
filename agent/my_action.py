afd2b883ab5a101aafb333a15b4ef6712f96b1a4
stz2233 <2062726423@qq.com>
2025-04-04 05:41:37 +0800

    实现自动游园会

--- agent/my_action.py
+++ agent/my_action.py
@@ -1,8 +1,11 @@
 from maa.agent.agent_server import AgentServer
 from maa.custom_action import CustomAction
 from maa.context import Context
-
-
+num=66
+biao=[15,28,30,37,53,65]
+int_1=65
+dd=0
+deshu=False
 @AgentServer.custom_action("my_action_111")
 class MyCustomAction(CustomAction):
 
@@ -11,7 +14,22 @@ class MyCustomAction(CustomAction):
         context: Context,
         argv: CustomAction.RunArg,
     ) -> bool:
+        if(int_1 > num):
+            int_1=int_1-num
+        for cell in biao:
+            if cell -  int_1 > 0:
+                if cell -  int_1 < 6:
+                    dd=cell-int_1
+                    deshu=True
+        img = context.tasker.controller.post_screencap().wait().get()
+        if(deshu):
+            reco_detail = context.run_recognition(
+                "特殊投一次",
+                img,
+                )
+            
+            context.run_action( )
+        
 
-        print("my_action_111 is running!")
 
         return True
--- agent/my_reco.py
+++ agent/my_reco.py
@@ -1,11 +1,10 @@
 from maa.agent.agent_server import AgentServer
 from maa.custom_recognition import CustomRecognition
 from maa.context import Context
+import math
 
-
-@AgentServer.custom_recognition("my_reco_222")
-class MyRecongition(CustomRecognition):
-
+@AgentServer.custom_recognition("IsSimilar")
+class IsSimilar(CustomRecognition):
     def analyze(
         self,
         context: Context,
@@ -13,25 +12,30 @@ class MyRecongition(CustomRecognition):
     ) -> CustomRecognition.AnalyzeResult:
 
         reco_detail = context.run_recognition(
-            "MyCustomOCR",
+            "识别票",
             argv.image,
-            pipeline_override={"MyCustomOCR": {"roi": [100, 100, 200, 300]}},
-        )
-
+            )
+                        # reco_detail = context.run_recognition(
+        #     "",
+        #     argv.image,
+        #     pipeline_override={"MyCustomOCR": {"recognition": "OCR","expected": "投1次",
+        #                         "index": 1,
+        #                         }},
+        #     )
         # context is a reference, will override the pipeline for whole task
-        context.override_pipeline({"MyCustomOCR": {"roi": [1, 1, 114, 514]}})
+        # context.override_pipeline({"MyCustomOCR": {"roi": [1, 1, 114, 514]}})
         # context.run_recognition ...
-
-        # make a new context to override the pipeline, only for itself
-        new_context = context.clone()
-        new_context.override_pipeline({"MyCustomOCR": {"roi": [100, 200, 300, 400]}})
-        reco_detail = new_context.run_recognition("MyCustomOCR", argv.image)
+        # print(steps_manhattan)
+        # # make a new context to override the pipeline, only for itself
+        # new_context = context.clone()
+        # new_context.override_pipeline({"MyCustomOCR": {"roi": [100, 200, 300, 400]}})
+        # reco_detail = new_context.run_recognition("MyCustomOCR", argv.image)
 
         click_job = context.tasker.controller.post_click(10, 20)
         click_job.wait()
 
-        context.override_next(argv.node_name, ["TaskA", "TaskB"])
-
+        # context.override_next(argv.node_name, ["TaskA", "TaskB"])
+        
         return CustomRecognition.AnalyzeResult(
-            box=(0, 0, 100, 100), detail="Hello World!"
+            reco_detail.box, detail="Hello World!"
         )
